d60e5c5a7a74073f10174b487d81057b5ee2aadc
DurandA <arnaud.durand@live.com>
2017-08-23 19:45:52 +0200

    Add Redis storage

--- run_server.py
+++ run_server.py
@@ -2,67 +2,65 @@ import asyncio
 from aiohttp import web
 from aiohttp.web import Application, Response, json_response, HTTPNotAcceptable, View
 from aiohttp_sse import sse_response
+import aioredis
 
 from json import dumps
 from collections import defaultdict
 from itertools import cycle
 
+loop = asyncio.get_event_loop()
 
-class LEDCycle(object):
 
-    def __init__(self):
-        self.color_cycle = cycle(range(1,9))
+async def devices(request):
+    redis = request.app['redis']
+    return json_response(thingyset)
+    thingyset = await redis.smembers('thingy')
 
-    def __next__(self):
-        return {
-            'color': next(self.color_cycle),
-            'intensity': 20,
-            'delay': 1000
-        }
-
-led_cycle = LEDCycle()
-
-async def led(request):
-    if request.headers.get('EXPECT') == 'text/event-stream':
-        loop = request.app.loop
-        async with sse_response(request) as resp:
-            while True:
-                await asyncio.sleep(1, loop=loop)
-                resp.send(dumps(next(led_cycle)))
-        return resp
-    else:
-        return json_response(next(led_cycle))
+async def sensors(request):
+    pass
 
 class SensorView(View):
-    data = defaultdict(lambda : defaultdict(int))
+    key = 'value'
 
     def __init__(self, request):
         super().__init__(request)
-        self.device_id = request.match_info['device_id']
-        self.timestamp = request.match_info['timestamp']
+        self.thingy_id = request.match_info['thingy_id']
+        try:
+            self.timestamp = int(request.match_info['timestamp'])
+        except KeyError:
+            self.timestamp = None
+        self.redis = request.app['redis']
 
     async def get(self):
-        return json_response({'value': self.data[self.device_id][self.timestamp]})
+        if self.timestamp is not None:
+            val = await self.redis.zrevrangebyscore(self.thingy_id+':'+self.key, self.timestamp, self.timestamp)
+            return json_response(body=val[0] if val else '{}')
+        else:
+            val = await self.redis.zrevrange(self.thingy_id+':'+self.key, 0, -1)
+            return json_response(body='['+','.join(val)+']')
 
     async def put(self):
-        sensor = await self.request.json()
-        self.data[self.device_id][self.timestamp] = sensor['value']
-        return json_response({'value': sensor['value']})
+        data = await self.request.json()
+        tr = self.redis.multi_exec()
+        tr.zadd(self.thingy_id+':'+self.key, self.timestamp, dumps(data))
+        tr.sadd('thingy', self.thingy_id)
+        await tr.execute()
+        return json_response(data)
 
 class TemperatureView(SensorView):
-    pass
+    key = 'temperature'
 
 class PressureView(SensorView):
-    pass
+    key = 'pressure'
 
 class HumidityView(SensorView):
-    pass
+    key = 'humidity'
 
 class GasView(SensorView):
-    pass
+    key = 'gas'
 
 class ColorView(SensorView):
-    pass
+    key = 'color'
 
 class ButtonView(View):
     states = {}
@@ -92,16 +90,57 @@ async def setup(request):
     }
     return json_response(settings)
 
-app = web.Application()
+class LEDCycle(object):
 
-app.router.add_route('*', '/{device_id}/temperature/{timestamp}', TemperatureView)
-app.router.add_route('*', '/{device_id}/pressure/{timestamp}', PressureView)
-app.router.add_route('*', '/{device_id}/humidity/{timestamp}', HumidityView)
-app.router.add_route('*', '/{device_id}/gas/{timestamp}', GasView)
-app.router.add_route('*', '/{device_id}/color/{timestamp}', ColorView)
-app.router.add_route('*', '/{device_id}/button', button)
+    def __init__(self):
+        self.color_cycle = cycle(range(1,9))
 
-app.router.add_get('/{device_id}/led', led)
-app.router.add_get('/{device_id}/setup', setup)
+    def __next__(self):
+        return {
+            'color': next(self.color_cycle),
+            'intensity': 20,
+            'delay': 1000
+        }
 
-web.run_app(app, host='127.0.0.1', port=8080)
+led_cycle = LEDCycle()
+
+async def led(request):
+    if request.headers.get('EXPECT') == 'text/event-stream':
+        async with sse_response(request) as resp:
+            while True:
+                await asyncio.sleep(1, loop=loop)
+                resp.send(dumps(next(led_cycle)))
+        return resp
+    else:
+        return json_response(next(led_cycle))
+
+async def init(loop):
+    app = web.Application(loop=loop)
+    app['redis'] = await aioredis.create_redis(
+            ('localhost', 6379), encoding='utf-8', loop=loop)
+
+    app.router.add_get('/', devices)
+
+
+    app.router.add_route('post', '/{thingy_id}/sensors/', sensors)
+    app.router.add_route('*', '/{thingy_id}/sensors/{sensor:temperature|pressure|humidity|gas|color}', SensorView)
+    app.router.add_route('*', '/{thingy_id}/temperature/{timestamp:\d+}', TemperatureView, name='temperature')
+    app.router.add_route('*', '/{thingy_id}/pressure/{timestamp:\d+}', PressureView)
+    app.router.add_route('*', '/{thingy_id}/humidity/{timestamp:\d+}', HumidityView)
+    app.router.add_route('get', '/{thingy_id}/humidity/', HumidityView)
+    app.router.add_route('*', '/{thingy_id}/gas/{timestamp:\d+}', GasView)
+    app.router.add_route('*', '/{thingy_id}/color/{timestamp:\d+}', ColorView)
+    app.router.add_route('*', '/{thingy_id}/button', button)
+
+    app.router.add_get('/{device_id}/led', led)
+    app.router.add_get('/{device_id}/setup', setup)
+
+    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 8080)
+    return srv
+
+loop.run_until_complete(init(loop))
+
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
